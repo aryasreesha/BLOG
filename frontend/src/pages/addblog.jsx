11c6ad9d634b613b9b2487e4866f0b1a946df737
aryasreesha <165008399+aryasreesha@users.noreply.github.com>
2024-04-18 01:07:52 -0700

    Add files via upload

---
+++ frontend/src/pages/addblog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BlogForm from '../components/form/blogform';
+
+const AddBlog = () => {
+  return (
+    <div>
+    <BlogForm/>
+ </div>
+  )
+}
+export default AddBlog;
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Myblog from '../components/blog/myblog'
+
+const Home = () => {
+  return (
+    <div>
+
+<Myblog />
+    </div>
+  )
+}
+export default Home;
